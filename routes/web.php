0abfde1c0cf7ea012fc12a1d1f743a2f4487e605
FraRussotto <russottofrancesco1@gmail.com>
2023-11-16 16:52:59 +0100

    [edit] bonus

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $pages_links = ['home', 'team', 'about-us'];
+    return view('nav', compact('pages_links'));
+});
+Route::get('/home', function () {
     $first_word = 'Hello';
     $second_word = 'World';
+    $pages_links = ['home', 'team', 'about-us'];
     return view('home', compact('first_word', 'second_word'));
+});
+Route::get('/team', function () {
+    $first_word = 'Team';
+    $pages_links = ['home', 'team', 'about-us'];
+    return view('team', compact('first_word'));
+});
+Route::get('/about-us', function () {
+    $first_word = 'About-us';
+    $pages_links = ['home', 'team', 'about-us'];
+    return view('about-us', compact('first_word'));
 });
